c7b2284bf0ac05ebf3cb06e8e278520211f2448d
shafin4201 <arinisshafin@gmail.com>
2025-06-13 18:02:28 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-echo "Installing Chrome..."
-apt-get update && apt-get install -y wget unzip curl
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb || apt --fix-broken install -y
-
-echo "Installing ChromeDriver..."
-CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
-wget -N https://chromedriver.storage.googleapis.com/$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-mv chromedriver /usr/bin/chromedriver
-chmod +x /usr/bin/chromedriver
-
-echo "Running script..."
+Xvfb :99 -screen 0 1024x768x16 &  # Start virtual display
+sleep 2
 python3 main.py
